3cf84c08f422118f8bf6305b783e3f7cd967ec77
tmerlier <tmerlier@student.42.fr>
2019-09-30 10:07:40 +0200

    Add FAQ

---
+++ components/head/index.js
@@ -0,0 +1,69 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+import theme from '../../styles/theme'
+
+import Container from '../container'
+
+import BetaRibbon from './beta-ribbon'
+
+const Head = ({ title, icon, beta }) => (
+  <div>
+    <div className='head'>
+      <Container>
+        <div className='flex'>
+          <div className='title'>
+            <div className='icon'>{icon}</div>
+            <h1>{title}</h1>
+          </div>
+
+          {beta && (
+            <BetaRibbon />
+          )}
+        </div>
+      </Container>
+    </div>
+
+    <style jsx>{`
+      .head {
+        background-color: ${theme.primary};
+      }
+
+      .flex {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+      }
+
+      .title {
+        display: flex;
+        align-items: center;
+        max-width: 1400px;
+        margin-top: 0;
+        color: #fff;
+        padding-bottom: 0.5em;
+      }
+
+      .icon {
+        font-size: 56px;
+      }
+
+      h1 {
+        margin-left: 1em;
+        margin-top: 1em;
+      }
+    `}</style>
+  </div>
+)
+
+Head.propTypes = {
+  title: PropTypes.string.isRequired,
+  icon: PropTypes.element.isRequired,
+  beta: PropTypes.bool
+}
+
+Head.defaultProps = {
+  beta: false
+}
+
+export default Head
---
+++ pages/faq.js
@@ -0,0 +1,54 @@
+import Link from 'next/link'
+import FaQuestion from 'react-icons/lib/fa/question'
+
+import Page from '../layouts/main'
+
+import Section from '../components/section'
+import Question from '../components/faq/question'
+import Head from '../components/head'
+
+export default () => (
+  <Page title='Foire aux questions'>
+    <Head title="Foire aux questions" icon={<FaQuestion />} />
+
+    <Section>
+      <div className='faq-row'>
+          <div className='theme'>
+            <h3>API de géocodage</h3>
+
+            <Question question='Quelle est la licence des données proposées par l’API de géocodage ?'>
+              <p>Les données utilisées par <Link href='/adresse'><a>l’API disponible sur ce site</a></Link> sont celles sous licence ODbL.</p>
+            </Question>
+
+            <Question question='Quelle sont les limitations en vigueur sur l’API de géocodage ?'>
+              <>
+                <p>Les appels sont limités à :</p>
+                <ul>
+                  <li>50 requêtes par seconde et par IP pour le géocodage simple ;</li>
+                  <li>2 requêtes simultanées par IP pour le géocodage de masse (maximum 6 Mo par envoi de fichier).</li>
+                </ul>
+              </>
+            </Question>
+
+            <Question question='Est-il possible de faire lever les limites de l’API ?'>
+              <>
+                <p>Oui, mais uniquement si vous êtes un service public ou chargé d’une mission de service public</p>
+                <p>Dans le cas contraire, vous pouvez aussi héberger notre API de géocodage chez vous, en suivant <a href='https://github.com/etalab/addok-docker'>ces instructions</a>.</p>
+              </>
+            </Question>
+          </div>
+
+          <style jsx>{`
+            .theme {
+              margin: 1em 0;
+            }
+
+            .faq-row {
+              display: flex;
+              flex-direction: column;
+            }
+          `}</style>
+        </div>
+    </Section>
+  </Page>
+)
