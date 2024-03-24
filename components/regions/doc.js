cb5c4ac5fc3fa9b3967d71cc606006bd72287904
Maxime Pauvert <maximepvrt@gmail.com>
2024-03-24 18:45:57 +0100

    Update regions doc

--- components/regions/doc.js
+++ components/regions/doc.js
@@ -1,20 +1,7 @@
 // Fields
 const nom = {name: 'nom', description: 'Nom de la région', type: 'string'}
 const code = {name: 'code', description: 'Code (INSEE) de la région', type: 'string'}
-const codeDepartement = {name: 'codeDepartement', description: 'Code du département associé', type: 'string'}
 const fields = {name: 'fields', description: 'Liste des champs souhaités dans la réponse', type: 'array [string]'}
-
-// Arguments
-const departement = {
-  name: 'departement',
-  description: 'Département associé à la commune',
-  type: 'object',
-  model: {
-    code: 'string',
-    nom: 'string',
-    codeRegion: 'string'
-  }
-}
 
 const _score = {
   name: '_score',
@@ -24,7 +11,7 @@ const _score = {
 
 const paths = [
   {name: '/regions', description: 'Rechercher des régions', params: [code, nom, fields]},
-  {name: '/regions/{code}', description: 'Récupérer les informations concernant une commune', params: [code, fields]}
+  {name: '/regions/{code}', description: 'Récupérer les informations concernant une région', params: [code, fields]}
 ]
 
 const defaultModel = [
@@ -37,12 +24,9 @@ const defaultModel = [
 const defaultAttributs = [
   code,
   nom,
-  codeDepartement,
   _score
 ]
 
-const optionAttributs = [
-  departement
-]
+const optionAttributs = []
 
 export default {paths, defaultModel, defaultAttributs, optionAttributs}
